ae77ac24efaccf6dfc3e9bac42b9269a2ecebc39
rraminen <rraminen@amd.com>
2023-04-20 18:09:04 -0400

    Workaround for ROCm

--- csrc/transformer/inference/csrc/apply_rotary_pos_emb.cu
+++ csrc/transformer/inference/csrc/apply_rotary_pos_emb.cu
@@ -190,10 +190,17 @@ __global__ void apply_rotary_pos_emb1(__half* mixed_query,
             float rotary_sign = (lane > (half_dim - 1) ? -1.0 : 1.0);
             float q_rot = (q * rotary_sign);
             float k_rot = (k * rotary_sign);
+#if defined(__HIP_PLATFORM_HCC__)
+            auto q_rot_tmp = lane < half_dim ? __shfl(q_rot, lane + half_dim)
+                                             : __shfl(q_rot, lane - half_dim);
+            auto k_rot_tmp = lane < half_dim ? __shfl(k_rot, lane + half_dim)
+                                             : __shfl(k_rot, lane - half_dim);
+#else
             auto q_rot_tmp = lane < half_dim ? __shfl_sync(mask[lane], q_rot, lane + half_dim)
                                              : __shfl_sync(mask[lane], q_rot, lane - half_dim);
             auto k_rot_tmp = lane < half_dim ? __shfl_sync(mask[lane], k_rot, lane + half_dim)
                                              : __shfl_sync(mask[lane], k_rot, lane - half_dim);
+#endif
             q = q * cosf(inv_freq) + q_rot_tmp * sinf(inv_freq);
             k = k * cosf(inv_freq) + k_rot_tmp * sinf(inv_freq);
 
